f32ab77ea9744f405938ecb2b1835c8994763db3
PhD20 <dmprofessor227@gmail.com>
2022-11-09 18:49:19 -0600

    Working tests

---
+++ src/utils/__tests__/isValidUrl.test.ts
@@ -0,0 +1,10 @@
+import { isValidUrl } from "../util";
+
+describe('isValidU', () => {
+    test('returns false for invalid url', () => {
+      expect(isValidUrl('invalid-url.com')).toBe(false);
+    });
+    test('returns true for valid url', () => {
+        expect(isValidUrl('https://www.invalid-url.com')).toBe(false);
+      });
+  });
