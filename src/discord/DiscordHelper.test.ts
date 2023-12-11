418a02fabf4d6f67d6c1b25ac0cbdd4386cc6008
PhD20 <dmprofessor227@gmail.com>
2023-12-10 19:09:37 -0600

    Linting and extra tests

--- src/discord/DiscordHelper.test.ts
+++ src/discord/DiscordHelper.test.ts
@@ -1,6 +1,6 @@
 import DiscordHelper from './DiscordHelper';
 import { TFile } from 'obsidian';
-import { DiscordEmbedColor, DiscordEmbedTitle } from './constants';
+import { DiscordEmbedAuthor, DiscordEmbedColor, DiscordEmbedDescription, DiscordEmbedFields, DiscordEmbedFooter, DiscordEmbedImage, DiscordEmbedThumbnail, DiscordEmbedTitle, DiscordEmbedURL } from './constants';
 
 // Mocking dependencies
 const mockPlugin = {
@@ -14,15 +14,18 @@ const mockPlugin = {
       },
     },
   },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } as any;
 
-const mockSettings = {} as any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let mockSettings = {} as any;
 
 describe('DiscordHelper', () => {
   let discordHelper: DiscordHelper;
 
   beforeEach(() => {
     discordHelper = new DiscordHelper(mockPlugin);
+    mockSettings = {};
   });
 
   describe('buildDiscordEmbedParamsFromFile', () => {
@@ -43,9 +46,15 @@ describe('DiscordHelper', () => {
       } as TFile;
 
       const mockFrontmatter = {
-        [DiscordEmbedColor]: 'defaultColor',
         [DiscordEmbedTitle]: 'defaultTitle',
-        // ... add other default values
+        [DiscordEmbedColor]: 'defaultColor',
+        [DiscordEmbedURL]: 'defaultUrl',
+        [DiscordEmbedAuthor]: 'defaultAuthor',
+        [DiscordEmbedDescription]: 'defaultDescription',
+        [DiscordEmbedThumbnail]: 'defaultThumbnail',
+        [DiscordEmbedFields]: 'defaultFields',
+        [DiscordEmbedImage]: 'defaultImage',
+        [DiscordEmbedFooter]: 'defaultFooter',
       };
 
       mockPlugin.app.metadataCache.getFileCache.mockReturnValueOnce({
@@ -57,7 +66,106 @@ describe('DiscordHelper', () => {
       expect(result).toEqual({
         color: 'defaultColor',
         title: 'defaultTitle',
-        // ... add other expected values
+        url: 'defaultUrl',
+        author: 'defaultAuthor',
+        description: 'defaultDescription',
+        thumbnail: 'defaultThumbnail',
+        fields: 'defaultFields',
+        image: 'defaultImage',
+        footer: 'defaultFooter',
+        file: mockFile,
+      });
+    });
+
+    it('uses the global color setting if none exists in frontmatter', () => {
+      const mockFile = {
+        basename: 'TestFile',
+      } as TFile;
+
+      const mockFrontmatter = {
+        [DiscordEmbedTitle]: 'defaultTitle',
+      };
+
+      mockPlugin.app.metadataCache.getFileCache.mockReturnValueOnce({
+        frontmatter: mockFrontmatter,
+      });
+
+      mockSettings.embedColor = "red";
+
+      const result = discordHelper.buildDiscordEmbedParamsFromFile(mockFile, mockSettings);
+
+      expect(result?.color).toBe("red");
+    });
+
+    it('uses the frontmatter color setting over the global setting', () => {
+      const mockFile = {
+        basename: 'TestFile',
+      } as TFile;
+
+      const mockFrontmatter = {
+        [DiscordEmbedColor]: 'blue',
+        [DiscordEmbedTitle]: 'defaultTitle',
+      };
+
+      mockPlugin.app.metadataCache.getFileCache.mockReturnValueOnce({
+        frontmatter: mockFrontmatter,
+      });
+
+      mockSettings.embedColor = "red";
+
+      const result = discordHelper.buildDiscordEmbedParamsFromFile(mockFile, mockSettings);
+
+      expect(result?.color).toBe("blue");
+    });
+
+    it('builds Discord embed params with override values', () => {
+      const mockFile = {
+        basename: 'TestFile',
+      } as TFile;
+
+      const mockFrontmatter = {
+        [DiscordEmbedTitle]: 'defaultTitle',
+        [DiscordEmbedURL]: 'defaultUrl',
+        [DiscordEmbedAuthor]: 'defaultAuthor',
+        [DiscordEmbedDescription]: 'defaultDescription',
+        [DiscordEmbedThumbnail]: 'defaultThumbnail',
+        [DiscordEmbedFields]: 'defaultFields',
+        [DiscordEmbedImage]: 'defaultImage',
+        [DiscordEmbedFooter]: 'defaultFooter',
+        "override-title": 'overrideTitle',
+        "override-url": 'overrideUrl',
+        "override-author": 'overrideAuthor',
+        "override-description": 'overrideDescription',
+        "override-thumbnail": 'overrideThumbnail',
+        "override-fields": 'overrideFields',
+        "override-image": 'overrideImage',
+        "override-footer": 'overrideFooter'
+      };
+
+      mockPlugin.app.metadataCache.getFileCache.mockReturnValueOnce({
+        frontmatter: mockFrontmatter,
+      });
+
+      mockSettings.embedTitle = "override-title";
+      mockSettings.embedURL = "override-url";
+      mockSettings.embedAuthor = "override-author";
+      mockSettings.embedDescription = "override-description";
+      mockSettings.embedThumbnail = "override-thumbnail";
+      mockSettings.embedFields = "override-fields";
+      mockSettings.embedImage = "override-image";
+      mockSettings.embedFooter = "override-footer";
+
+      const result = discordHelper.buildDiscordEmbedParamsFromFile(mockFile, mockSettings);
+
+      expect(result).toEqual({
+        title: 'overrideTitle',
+        url: 'overrideUrl',
+        author: 'overrideAuthor',
+        description: 'overrideDescription',
+        thumbnail: 'overrideThumbnail',
+        fields: 'overrideFields',
+        image: 'overrideImage',
+        footer: 'overrideFooter',
         file: mockFile,
       });
     });
