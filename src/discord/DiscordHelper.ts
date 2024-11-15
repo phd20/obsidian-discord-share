9efed07b86859bf51339ffa70c799c4a4fafa150
PhD20 <dmprofessor227@gmail.com>
2024-11-15 10:22:55 -0600

    Update DiscordHelper.ts

--- src/discord/DiscordHelper.ts
+++ src/discord/DiscordHelper.ts
@@ -29,6 +29,15 @@ export default class DiscordHelper {
 	}
 
 	public buildDiscordEmbedParamsFromFile(file: TFile, settings: ISettingsOptions) {
+
+		// Create embed from default values 
+
+		// Override with frontmatter values if they exist
+		// If a frontmatter override is found, use it to check the frontmatter for value. 
+		// If a frontmatter override is not found, use the default frontmatter key.
+		// If a default frontmatter key is not found, use the default value.
+		// Return embed
+
 		const metadata = this.metadata.getFileCache(file);
 		if (!metadata) {
 			console.log("No metadata found for file.");
@@ -40,65 +49,83 @@ export default class DiscordHelper {
 			return;
 		}
 
-		const { embedPropertyOverrides } = settings;
+		const { embedDefaultValues, embedPropertyOverrides } = settings;
+		const { embedDefaultAuthor, embedDefaultFooter } = embedDefaultValues;
 
+		// TODO - Refactor this to be more DRY
 		const getValueForParam = (param: string) => {
 			switch (param) {
 				case "color":
 					if (frontmatter[DiscordEmbedColor]) {
 						return frontmatter[DiscordEmbedColor];
 					} else {
-						return embedPropertyOverrides.embedColorPropertyOverride;
+						return embedDefaultValues.embedDefaultColor;
 					}
 				case "title":
 					if (settings.useNoteTitleForEmbed) {
 						return file.basename;
 					} else if (embedPropertyOverrides.embedTitlePropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedTitlePropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedTitle]) {
 						return frontmatter[DiscordEmbedTitle];
+					} else {
+						return embedDefaultValues.embedDefaultTitle;
 					}
 				case "url":
 					if (embedPropertyOverrides.embedURLPropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedURLPropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedURL]) {
 						return frontmatter[DiscordEmbedURL];
+					} else {
+						return embedDefaultValues.embedDefaultURL;
 					}
 				case "author":
 					if (embedPropertyOverrides.embedAuthorPropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedAuthorPropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedAuthor]) {
 						return frontmatter[DiscordEmbedAuthor];
+					} else {
+						return embedDefaultAuthor; // TODO - Test that this works with object
 					}
 				case "description":
 					if (embedPropertyOverrides.embedDescriptionPropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedDescriptionPropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedDescription]) {
 						return frontmatter[DiscordEmbedDescription];
+					} else {
+						return embedDefaultValues.embedDefaultDescription;
 					}
 				case "thumbnail":
 					if (embedPropertyOverrides.embedThumbnailPropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedThumbnailPropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedThumbnail]) {
 						return frontmatter[DiscordEmbedThumbnail];
+					} else {
+						return embedDefaultValues.embedDefaultThumbnail;
 					}
 				case "fields":
 					if (embedPropertyOverrides.embedFieldsPropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedFieldsPropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedFields]) {
 						return frontmatter[DiscordEmbedFields];
+					} else {
+						return embedDefaultValues.embedDefaultFields;
 					}
 				case "image":
 					if (embedPropertyOverrides.embedImagePropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedImagePropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedImage]) {
 						return frontmatter[DiscordEmbedImage];
+					} else {
+						return embedDefaultValues.embedDefaultImage;
 					}
 				case "footer":
 					if (embedPropertyOverrides.embedFooterPropertyOverride) {
 						return frontmatter[embedPropertyOverrides.embedFooterPropertyOverride];
-					} else {
+					} else if (frontmatter[DiscordEmbedFooter]) {
 						return frontmatter[DiscordEmbedFooter];
+					} else {
+						return embedDefaultFooter; // TODO - Test that this works with object
 					}
 				default:
 					break;
@@ -117,6 +144,8 @@ export default class DiscordHelper {
 			footer: getValueForParam("footer"),
 			file: file,
 		}
+
+		console.log(embedParams);
 
 		return embedParams;
 	}
